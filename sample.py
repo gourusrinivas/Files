80cc8eebcf90e9c3948b70e79fec83a81d6ff509
Gouru Srinivas <46845166+gourusrinivas@users.noreply.github.com>
2022-01-28 21:33:39 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -7,3 +7,25 @@ files = sorted(os.listdir(os.getcwd()), key=os.path.getmtime)
 recent = files[-5:]
 
 print(recent)
+
+import os
+from pathlib import Path
+from zipfile import ZipFile
+
+
+DOWNLOAD_DIR = Path("D:\Hello")
+ZIPPED_FILE_DIR = Path("D:\Hello")
+
+
+def get_list_of_all_folders(download_dir: Path):
+    return [f for f in download_dir.iterdir() if download_dir.is_dir()]
+
+
+def zip_files():
+    folder_list = get_list_of_all_folders(DOWNLOAD_DIR)
+    with ZipFile(ZIPPED_FILE_DIR / "Other.zip", "w") as zip:
+        # writing each file one by one
+        for folder in folder_list:
+            zip.write(folder)
+           
+zip_files()
