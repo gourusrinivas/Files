be796c038a754b4ee3b4d0ec1d0fe159d9b87fed
Gouru Srinivas <46845166+gourusrinivas@users.noreply.github.com>
2022-01-29 12:53:26 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -4,7 +4,8 @@ path='D:\Hello'
 os.chdir(path)
 files = sorted(os.listdir(os.getcwd()), key=os.path.getmtime)
 
-a = float(input(files[:5]))
+x = files[:5]
+a = float(input(x))
 path = pathlib.path(a)
 path.unlink()
 
