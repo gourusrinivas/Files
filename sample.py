fe69bda0f21ebbe44c152fe572b6a1c644b49a31
Gouru Srinivas <46845166+gourusrinivas@users.noreply.github.com>
2022-01-30 00:57:04 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -6,9 +6,9 @@ files = sorted(os.listdir(os.getcwd()), key=os.path.getmtime)
 
 recent = files.reverse()
 
-for f in files[5:]:
+for f in files[4:]:
     os.remove(os.path.join(path, f))
-for f in files[:5]:
+for f in files[:6]:
     
     print(f)
 
