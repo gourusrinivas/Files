c5227c7e1366b5bd45573fa642016a3df6fabb44
Gouru Srinivas <46845166+gourusrinivas@users.noreply.github.com>
2022-01-30 21:19:15 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -13,10 +13,13 @@ print("RECENT FILES ARE")
 for i in f[:5]:
     print(i)
 
+import pandas as pd
 import os, zipfile
 
+exzip = zipfile.ZipFile('D:\Hello')
+df = pd.read_csv(exzip.open('First.csv'))
+
 os.chdir('D:\Hello')
-exzip = zipfile.ZipFile('recent.zip','w')
-exzip = zipfile.ZipFile('recent.zip','r')
+exzip = zipfile.ZipFile('recent.zip', 'w')
 
 exzip.close()
