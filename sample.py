95ef9663d01a111fbfd6db2839edaa20af999c52
Gouru Srinivas <46845166+gourusrinivas@users.noreply.github.com>
2022-01-30 20:55:02 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,4 +1,4 @@
-import os
+import os, zipfile
 
 os.chdir('D:\Hello')
 f = sorted(os.listdir(os.getcwd()), key=os.path.getmtime)
@@ -13,26 +13,6 @@ print("RECENT FILES ARE")
 for i in f[:5]:
     print(i)
 
-
-
-import os
-from pathlib import Path
-from zipfile import ZipFile
-
-
-DOWNLOAD_DIR = Path("D:\Hello")
-ZIPPED_FILE_DIR = Path("D:\Hello")
-
-
-def get_list_of_all_folders(download_dir: Path):
-    return [f for f in download_dir.iterdir() if download_dir.is_dir()]
-
-
-def zip_files():
-    folder_list = get_list_of_all_folders(DOWNLOAD_DIR)
-    with ZipFile(ZIPPED_FILE_DIR / "Other.zip", "w") as zip:
-        # writing each file one by one
-        for folder in folder_list:
-            zip.write(folder)
-           
-zip_files()
+os.chdir('D:\Hello')
+exzip = zipfile.ZipFile('recent.zip','w')
+exzip.close()
