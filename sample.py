ffb0f47986c0c875d58cd1bdbad184a882ab3bf3
Gouru Srinivas <46845166+gourusrinivas@users.noreply.github.com>
2022-01-30 20:16:54 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -5,7 +5,7 @@ f = sorted(os.listdir(os.getcwd()), key=os.path.getmtime)
 
 f.reverse()
 
-for i in f[6:]:
+for i in f[5:]:
     os.remove(os.path.join('D:\Hello', i))
     
 print("RECENT FILES ARE")
