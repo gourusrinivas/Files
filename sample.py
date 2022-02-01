e79837186f9000bde07635c0987d6beb9b318115
Gouru Srinivas <46845166+gourusrinivas@users.noreply.github.com>
2022-02-01 14:48:35 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,13 +1,16 @@
 import os
 import zipfile
+import json
 
-os.chdir('D:\Hello')
+f = open('Data.json')
+data = json.load(f)
+os.chdir('data')
 f = sorted(os.listdir(os.getcwd()), key=os.path.getmtime)
 
 f.reverse()
 
 for i in f[5:]:
-    os.remove(os.path.join('D:\Hello', i))
+    os.remove(os.path.join('data', i))
     
 print("RECENT FILES ARE")
 
