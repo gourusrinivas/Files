4d55dbf88c234566a850865eaef7471474f4ea77
Gouru Srinivas <46845166+gourusrinivas@users.noreply.github.com>
2022-02-04 12:04:20 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -3,7 +3,6 @@ import zipfile
 
 
 
-
 os.chdir('D:\Hello')
 f = sorted(os.listdir(os.getcwd()), key=os.path.getmtime)
 
@@ -17,7 +16,7 @@ print("RECENT FILES ARE")
 for i in f[:5]:
     print(i)
 
-    
+
 with zipfile.ZipFile('Recent_Files.zip', 'w') as zipF:
     for file in f[:5]:
         zipF.write(file, compress_type=zipfile.ZIP_DEFLATED)
